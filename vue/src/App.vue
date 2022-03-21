0ad15fbdf4584637ef26fc47e6503f95f62f01e8
Rob Taylor <roboncode@gmail.com>
2022-03-21 12:25:03 -0600

    updated vue with sdk 2.0

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -2,19 +2,54 @@
 import { onMounted, ref } from 'vue';
 import * as bridge from '@arrowhealth/bridge-sdk'
 
+const authUser = ref({})
+const appConfig = ref({})
+const appStatus = ref({})
 const patient = ref({})
-const session
 
-onMounted(async () => {
-  patient.value = await bridge.getPatientInfo()
-  bridge.onPatientChanged(patientInfo => patient.value = patientInfo)
+onMounted(() => {
+  bridge.getAuthUser().then(v => authUser.value = v)
+  bridge.getAppStatus().then(v => appStatus.value = v)
+  bridge.getPatientInfo().then(v => patient.value = v)
+  bridge.onPatientChanged(v => patient.value = v)
 })
 </script>
 
 <template>
   <h2>Bridge Starter (Vue)</h2>
-  <strong>PATIENT INFO</strong>
-  <pre><code>{{JSON.stringify(patient, null, 2)}}</code></pre>
+
+  <strong>AUTH USER</strong>
+  <pre><code>{{ JSON.stringify(authUser, null, 2) }}</code></pre>
+
+  <hr />
+
+  <strong>APP STATUS</strong>
+  <pre><code>{{ JSON.stringify(appStatus, null, 2) }}</code></pre>
+
+  <hr />
+
+  <strong>PATIENT</strong>
+  <pre><code>{{ JSON.stringify(patient, null, 2) }}</code></pre>
+
+  <hr />
+
+  <div class="container">
+    <button @click="bridge.setBadgeCount(1)">Set Badge Count</button>
+    <button @click="bridge.setBadgeCount(0)">Clear Badge Count</button>
+  </div>
+
+  <hr />
+
+  <div class="container">
+    <button @click="bridge.showTile()">Show Tile</button>
+    <button @click="bridge.hideTile()">Hide Tile</button>
+  </div>
+
+  <hr />
+
+  <div class="container">
+    <button @click="bridge.closeApp()">Close App</button>
+  </div>
 </template>
 
 <style>
@@ -24,5 +59,12 @@ onMounted(async () => {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   margin-top: 10px;
+}
+
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 8px;
+  margin-bottom: 8px;
 }
 </style>
